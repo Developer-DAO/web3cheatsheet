d88ba9d4bd8580577d2666a52d591a9c83652d4a
Dev Tripathi <tripathii.devv@gmail.com>
2022-04-30 12:43:12 +0530

    Style: updated favicon
    Updated the Favicon of sizes 96x96, 32x32, 16x16.

--- theme.config.js
+++ theme.config.js
@@ -50,19 +50,19 @@ export default {
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/public/d_d-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="/favicon-96x96.png"
+        href="/public/d_d-96x96.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/public/d_d-16x16.png"
       />
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
